3dd01a7522856aeaa5c153fbf81d8bbd79e5ae79
Guillermo Guemez <guillermo@guemez.net>
2026-09-17 14:01:38 -0600

    Runic cask 0.62.0

--- Casks/runic.rb
+++ Casks/runic.rb
@@ -1,6 +1,6 @@
 cask "runic" do
-  version "0.61.2"
-  sha256 "86548d262898752695fb778746b8b3094a16b89ec95f3317512638cb8607d031"
+  version "0.62.0"
+  sha256 "95653b9fec01680cc399319441de3270be051d4abd7929eff529d6cfe2fd2e9c"
 
   url "https://dl.runic.guemez.net/Runic-#{version}.dmg",
       verified: "dl.runic.guemez.net/"
